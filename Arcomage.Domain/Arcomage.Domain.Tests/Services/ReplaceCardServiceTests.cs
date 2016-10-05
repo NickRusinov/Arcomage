a9f433615bece26179893dd87a7912b90e47a844
Николай <gultuy00@gmail.com>
2016-10-05 23:18:38 +0300

    Add service implementation of replace card from deck

---
+++ Arcomage.Domain/Arcomage.Domain.Tests/Services/ReplaceCardServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arcomage.Domain.Entities;
+using Arcomage.Domain.Services;
+using Moq;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace Arcomage.Domain.Tests.Services
+{
+    public class ReplaceCardServiceTests
+    {
+        [Theory, AutoFixture]
+        public void ReplaceCardTest(Mock<Card> cardMock,
+            [Frozen] Player player,
+            [Frozen] Mock<CardDeck> cardDeckMock,
+            ReplaceCardService sut)
+        {
+            var oldCard = player.CardSet.Cards[1];
+            var newCard = cardMock.Object;
+            cardDeckMock.Setup(cd => cd.PopCard(It.IsAny<Game>())).Returns(newCard);
+
+            sut.ReplaceCard(1);
+
+            cardDeckMock.Verify(cd => cd.PopCard(It.IsAny<Game>()), Times.Once);
+            cardDeckMock.Verify(cd => cd.PushCard(It.IsAny<Game>(), oldCard), Times.Once);
+            Assert.Equal(newCard, player.CardSet.Cards[1]);
+        }
+    }
+}
---
+++ Arcomage.Domain/Arcomage.Domain/Services/ReplaceCardService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arcomage.Domain.Entities;
+
+namespace Arcomage.Domain.Services
+{
+    public class ReplaceCardService : IReplaceCardService
+    {
+        private readonly Game game;
+
+        private readonly Player player;
+
+        public ReplaceCardService(Game game, Player player)
+        {
+            this.game = game;
+            this.player = player;
+        }
+
+        public void ReplaceCard(int cardIndex)
+        {
+            var oldCard = player.CardSet.Cards[cardIndex];
+
+            game.CardDeck.PushCard(game, oldCard);
+            var newCard = game.CardDeck.PopCard(game);
+
+            player.CardSet.Cards[cardIndex] = newCard;
+        }
+    }
+}
